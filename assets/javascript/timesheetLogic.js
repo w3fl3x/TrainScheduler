983a23be6c7dbf2cf7be7739c50a8886324ded84
Huy Nguyen <huy.m.nguyen@live.com>
2019-07-10 13:49:56 -0700

    Added form group and layout index.html. Added firebase configuration to timesheetLogic.js.

--- assets/javascript/timesheetLogic.js
+++ assets/javascript/timesheetLogic.js
@@ -1,1 +1,14 @@
 //Initialize Firebase
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBEHlnQoWHNnbMjiizaWzWyZJ4jv6NOoSQ",
+    authDomain: "train-schedule-hw-dd48c.firebaseapp.com",
+    databaseURL: "https://train-schedule-hw-dd48c.firebaseio.com",
+    projectId: "train-schedule-hw-dd48c",
+    storageBucket: "",
+    messagingSenderId: "1088296213702",
+    appId: "1:1088296213702:web:a7014f7783395d5e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
